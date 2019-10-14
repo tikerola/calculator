1a790696b6e87a260350015c02a3acaab4aad746
timo kerola <timo.kerola@edu.bc.fi>
2019-10-14 12:06:59 +0300

    toka

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './app.css'
+import './App.css'
 import drawNumberOrOperator from './helpers/hitbox'
 import screenCoords from './helpers/screenCoords'
 
