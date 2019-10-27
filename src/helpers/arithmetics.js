391320d287446249c75e8ef01715abee31eb2490
timo kerola <timo.kerola@edu.bc.fi>
2019-10-27 19:29:05 +0200

    arithmetics-tiedostoon exportit

--- src/helpers/arithmetics.js
+++ src/helpers/arithmetics.js
@@ -2,7 +2,7 @@
 
 //+ (summa)
 //muuttujaan tulos summataan for lausekkeessa jokainen taulukon luvun alkio
-function summa ( luvut[] ) {
+export function summa ( luvut ) {
     let tulos;
         for (let luku of tulos) {
             tulos += luku;
@@ -14,7 +14,7 @@ function summa ( luvut[] ) {
 //Siis taulukon ensimmäisestä vähennetään jokaisella kierroksella seuraava
 // joka jää tulokseen, josta taas
 //vähennetään seuraava
-function erotus ( luvut[] ) {
+export function erotus ( luvut ) {
     let tulos = luvut[0]; //tästä vähennetään taulukon seuraavat luvut
     //kiepin pitää alkaa taulukon [1]:stä eteenpäin
     for (let luku of luvut) {
@@ -26,16 +26,16 @@ function erotus ( luvut[] ) {
 //x (tulo)
 //taulukon [0] kerrotaan ensin yhdellä (joka on valmiina tulos muuttujassa)
 //sitten taulukon [1] x muuttujan tulo arvolla joka on [0]x[1] jne taulukon loppuun) 
-function tulo ( luvut[] ) {
+export function tulo ( luvut ) {
     let tulos = 1;
     for ( let luku of luvut) {
         tulos *= luku;
     }
-
-}return tulos;
+    return tulos;
+}
 
 // (osamäärä) lasku
-function osamaara ( eka, toka) {
+export function osamaara ( eka, toka) {
     let tulos = eka / toka;
     return tulos;
 }
